eb310478285761798749bfdb195705c1fb4013f3
Selen Manioglu <selenm@bs-mbpr192.d.ethz.ch>
2021-01-19 15:16:32 +0100

    Image in .csv format and intensity map visualization

---
+++ img_arr.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 14:21:11 2021
+
+@author: selenm
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+# load numpy array from csv file
+from numpy import loadtxt
+# load array
+img_arr = loadtxt('Img_scaled.csv', delimiter=',')
+# print the array
+#print(data)
+
+
+#------------------------------------------------------------------------------
+# Show the image array in grayscale color
+#------------------------------------------------------------------------------
+
+#img = Image.fromarray(np.uint8(img_arr * 255) , 'L')
+#img.show()
+
+#img = Image.fromarray(np.uint8(img_arr) , 'L')
+#img.show()
+
+
+#------------------------------------------------------------------------------
+# Show the image array as an intensity map
+#------------------------------------------------------------------------------
+
+#data = np.random.randint(0,12,(6,6)) # random values between 0 and 11
+
+fig, ax = plt.subplots()
+
+im = ax.imshow(img_arr)
+plt.colorbar(im, ax=ax)
+
+plt.show()
